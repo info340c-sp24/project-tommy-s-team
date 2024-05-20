87ab87e315642dffbdba3de714bd86a717217db3
TTsui123 <ttsui123@uw.edu>
2024-05-19 19:42:15 -0700

    fix 2

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,13 @@
 'use strict';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import AddTask_btn from './AddTask_btn';
-export default function Todo({taskList, setTaskList}) {
+import AddGroup_btn from './AddGroup_btn';
+
+export default function Todo({ taskList, setTaskList }) {
     function GetIncompletedTask(taskList) {
         return taskList.filter((task) => {
             return task.completed == false;
-        });  
+        });
     }
 
     return (
@@ -14,18 +16,24 @@ export default function Todo({taskList, setTaskList}) {
             <ol id="todo">
                 {taskList.map((task) => (
                     <li key={task.TaskID}>
-                        <input type="checkbox" className={`task-check-box ${task.TaskID}`} name="task-check-box" 
-                        onClick={() => {
-                            task.completed = true;
-                            setTaskList(GetIncompletedTask(taskList));
-                        }}
+                        <input
+                            type="checkbox"
+                            className={`task-check-box ${task.TaskID}`}
+                            name="task-check-box"
+                            onClick={() => {
+                                task.completed = true;
+                                setTaskList(GetIncompletedTask(taskList));
+                            }}
                         />
-                        <label htmlFor="taskInput" className="task-detail">{task.TaskName}</label><strong className="date">{task.DueDate}</strong> <button className="detail-btn"> Detail </button> <button className="delete-btn">Delete</button>
+                        <label htmlFor="taskInput" className="task-detail">{task.TaskName}</label>
+                        <strong className="date">{task.DueDate}</strong>
+                        <button className="detail-btn">Detail</button>
+                        <button className="delete-btn">Delete</button>
                     </li>
-                ))}   
+                ))}
             </ol>
-            <AddTask_btn taskList = {taskList} setTaskList={setTaskList} GetIncompletedTask={GetIncompletedTask}/>
+            <AddTask_btn taskList={taskList} setTaskList={setTaskList} GetIncompletedTask={GetIncompletedTask} />
+            <AddGroup_btn taskList={taskList} setTaskList={setTaskList} />
         </div>
-
     );
 }
